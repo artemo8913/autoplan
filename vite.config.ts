261196577b7f038f862b65572841d8de65a5d0fa
Artem <88224387+artemo8913@users.noreply.github.com>
2026-04-05 14:42:35 +0700

    Разбил билд на чанки

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,42 @@
 import path from "node:path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        react(),
+        visualizer({
+            open: true, // Automatically open the report in your browser
+            filename: "stats.html", // Output file name
+            gzipSize: true, // Show gzipped size
+            brotliSize: true, // Show brotli size
+        }),
+    ],
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "src"),
-        }
-    }
+        },
+    },
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks(id) {
+                    if (id.includes("node_modules")) {
+                        if (id.includes("@mantine") || id.includes("@floating-ui")) {
+                            return "ui-chunk";
+                        }
+                        if (id.includes("mobx")) {
+                            return "mobx-chunk";
+                        }
+                        if (id.includes("xlsx")) {
+                            return "xlsx-chunk";
+                        }
+                        return "vendor";
+                    }
+                },
+            },
+        },
+    },
 });
